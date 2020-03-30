ccb22c5e30bd7948cae2dd335b7dbe338f8bcfa7
maze-n <mmaz999@outlook.com>
2020-03-30 17:23:07 +0530

    Clean-up meson/post_install.py

--- meson/post_install.py
+++ meson/post_install.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 
 install_prefix = os.environ['MESON_INSTALL_PREFIX']
-schemadir = os.path.join(install_prefix, 'share/glib-2.0/schemas')
+schemadir = os.path.join(install_prefix, 'share', 'glib-2.0', 'schemas')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling the gsettings schemas...')
