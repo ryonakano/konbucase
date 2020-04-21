032725a54e503b038694d89f7be57d6aac5615f5
Ryo Nakano <26003928+ryonakano@users.noreply.github.com>
2020-04-21 20:36:48 +0900

    Add an app icon (#21)

--- meson/post_install.py
+++ meson/post_install.py
@@ -5,7 +5,11 @@ import subprocess
 
 install_prefix = os.environ['MESON_INSTALL_PREFIX']
 schemadir = os.path.join(install_prefix, 'share', 'glib-2.0', 'schemas')
+icon_cache_dir = os.path.join(install_prefix, 'share', 'icons', 'hicolor')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling the gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir])
+
+    print('Updating desktop icon cache…')
+    subprocess.call(['gtk-update-icon-cache', '-qtf', icon_cache_dir])
