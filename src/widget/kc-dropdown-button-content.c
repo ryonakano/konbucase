072fdf910e29bf413c5567e3032e8bb053f49442
Ryo Nakano <ryonakaknock3@gmail.com>
2026-06-14 08:43:06 +0900

    Fix missing property

--- src/widget/kc-dropdown-button-content.c
+++ src/widget/kc-dropdown-button-content.c
@@ -5,6 +5,16 @@
 
 #include "kc-dropdown-button-content.h"
 
+enum {
+    PROP_0,
+
+    PROP_LABEL_TEXT,
+
+    N_PROPS
+};
+
+static GParamSpec *props[N_PROPS];
+
 struct _KcDropDownButtonContent {
     AdwBin           parent_instance;
 
@@ -12,6 +22,43 @@ struct _KcDropDownButtonContent {
 };
 
 G_DEFINE_FINAL_TYPE (KcDropDownButtonContent, kc_dropdown_button_content, ADW_TYPE_BIN)
+
+static void
+kc_dropdown_button_content_get_property (GObject       *object,
+                                         guint          prop_id,
+                                         GValue        *value,
+                                         GParamSpec    *pspec)
+{
+    KcDropDownButtonContent *self = KC_DROPDOWN_BUTTON_CONTENT (object);
+
+    switch (prop_id) {
+    case PROP_LABEL_TEXT:
+        g_value_set_string (value, self->label_text);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+        break;
+    }
+}
+
+static void
+kc_dropdown_button_content_set_property (GObject       *object,
+                                         guint          prop_id,
+                                         const GValue  *value,
+                                         GParamSpec    *pspec)
+{
+    KcDropDownButtonContent *self = KC_DROPDOWN_BUTTON_CONTENT (object);
+
+    switch (prop_id) {
+    case PROP_LABEL_TEXT:
+        g_clear_pointer (&(self->label_text), g_free);
+        self->label_text = g_strdup (g_value_get_string (value));
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+        break;
+    }
+}
 
 static void
 kc_dropdown_button_content_dispose (GObject *object)
@@ -31,7 +78,14 @@ kc_dropdown_button_content_class_init (KcDropDownButtonContentClass *class)
 {
     GObjectClass *object_class = G_OBJECT_CLASS (class);
 
+    object_class->get_property = kc_dropdown_button_content_get_property;
+    object_class->set_property = kc_dropdown_button_content_set_property;
     object_class->dispose = kc_dropdown_button_content_dispose;
+
+    props[PROP_LABEL_TEXT] =
+        g_param_spec_string ("label-text", NULL, NULL,
+                             NULL,
+                             G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 }
 
 static void
