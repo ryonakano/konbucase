029fcfb135d35eb836e9b81ee6bb821392676dfd
Ryo Nakano <ryonakaknock3@gmail.com>
2026-06-18 20:31:51 +0900

    Review

--- src/model/kc-case-list-item.h
+++ src/model/kc-case-list-item.h
@@ -18,13 +18,13 @@ G_DECLARE_FINAL_TYPE (KcCaseListItem, kc_case_list_item, KC, CASE_LIST_ITEM, GOb
 extern KcCaseType kc_case_list_item_get_case_type (KcCaseListItem *self);
 extern void kc_case_list_item_set_case_type (KcCaseListItem *self, KcCaseType case_type);
 
-extern const gchar * kc_case_list_item_get_name (KcCaseListItem *self);
+extern const gchar *kc_case_list_item_get_name (KcCaseListItem *self);
 extern void kc_case_list_item_set_name (KcCaseListItem *self, const gchar *name);
 
-extern const gchar * kc_case_list_item_get_description (KcCaseListItem *self);
+extern const gchar *kc_case_list_item_get_description (KcCaseListItem *self);
 extern void kc_case_list_item_set_description (KcCaseListItem *self, const gchar *description);
 
-extern gboolean kc_case_list_item_equals (KcCaseListItem *a, KcCaseListItem *b);
+extern gboolean kc_case_list_item_equals (KcCaseListItem *self, KcCaseListItem *other);
 extern KcCaseListItem *kc_case_list_item_new (KcCaseType case_type, const gchar *name, const gchar *description);
 
 G_END_DECLS
